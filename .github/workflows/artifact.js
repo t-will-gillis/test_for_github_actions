f7d42b98898ac048f8eff1b9c9652b84c7db6e24
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2023-09-06 14:53:10 -0700

    Update artifact.js

--- .github/workflows/artifact.js
+++ .github/workflows/artifact.js
@@ -1,9 +1,21 @@
-const artifact = require('@actions/artifact');
-const artifactClient = artifact.create();
-const downloadResponse = await artifactClient.downloadAllArtifacts();
+// Import modules
+const fs = require("fs");
 
-// output result
-for (response in downloadResponse) {
+// Global variables
+var github;
+var context;
+
+async function main({ g, c }) {
+  github = g;
+  context = c;
+
+
+  const artifact = require('@actions/artifact');
+  const artifactClient = artifact.create();
+  const downloadResponse = await artifactClient.downloadAllArtifacts();
+
+  // output result
+  for (response in downloadResponse) {
     console.log(response.artifactName);
     console.log(response.downloadPath);
-}
+  }
